5ec90ac24042a01b593feeae79275de1b49ffc29
Rich Trott <rtrott@gmail.com>
2014-01-15 10:08:39 -0800

    install latest MySQL from base repo

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -4,3 +4,11 @@ include php
 include clamav
 include aspell
 include graphviz
+# include '::mysql::server'
+
+class { '::mysql::server':
+#    root_password    => 'strongpassword',
+#    override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+  package_ensure => 'latest',
+  service_enabled => 'true',
+}
--- manifests/prod.pp
+++ manifests/prod.pp
@@ -5,4 +5,11 @@ include clamav
 include aspell
 include graphviz
 
+class { '::mysql::server':
+#    root_password    => 'strongpassword',
+#    override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+  package_ensure => 'latest',
+  service_enabled => 'true',
+}
+
 include cron
