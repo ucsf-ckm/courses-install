fe456c7247bd2475351ad551d8ed978d23350953
Rich Trott <rtrott@gmail.com>
2014-01-15 20:51:52 -0800

    easier install of librarian-puppet and mysql

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-$PUPPET_DIR=/vagrant
+/bin/touch /etc/puppet/hiera.yaml
 
-/usr/bin/gem query | /bin/grep librarian-puppet || /usr/bin/gem install librarian-puppet
+/usr/bin/gem query | /bin/grep librarian-puppet || /usr/bin/gem install librarian-puppet --no-rdoc --no-ri
 
+PUPPET_DIR=/vagrant
 cd $PUPPET_DIR
+
 /usr/bin/librarian-puppet install
 
 /usr/bin/puppet apply --modulepath=$PUPPET_DIR/modules/ $PUPPET_DIR/manifests/dev.pp
--- modules.local/init/manifests/init.pp
+++ modules.local/init/manifests/init.pp
@@ -1,5 +1,10 @@
 class init {
-  exec { "yum update":
+  package { "yum-plugin-security":
+    ensure => "present",
+  }
+
+  exec { "yum update-minial":
     command => "/usr/bin/yum update-minimal -y",
+    require => Package["yum-plugin-security"],
   }
 }
